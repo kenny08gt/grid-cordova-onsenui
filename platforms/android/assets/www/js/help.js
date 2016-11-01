579f0b5e98bb4b0d6efbbdcfa838caf8dd80d170
Alan Hurtarte <alan220193@gmail.com>
2016-11-01 17:49:24 -0600

    Plugin FileSystema agregado

--- platforms/android/assets/www/js/help.js
+++ platforms/android/assets/www/js/help.js
@@ -1,3 +1,84 @@
 $("#btn-back").on("click",function() {
   window.location = "index.html";
 });
+
+function readFile() {
+   var type = window.TEMPORARY;
+   var size = 5*1024*1024;
+
+   window.requestFileSystem(type, size, successCallback, errorCallback)
+
+   function successCallback(fs) {
+
+      fs.root.getFile('log.txt', {}, function(fileEntry) {
+
+         fileEntry.file(function(file) {
+            var reader = new FileReader();
+
+            reader.onloadend = function(e) {
+              console.log(this.result);               
+            };
+
+            reader.readAsText(file);
+
+         }, errorCallback);
+
+      }, errorCallback);
+   }
+
+   function errorCallback(error) {
+      alert("ERROR: " + error.code)
+   }
+
+}
+
+function createFile() {
+   var type = window.TEMPORARY;
+   var size = 5*1024*1024;
+
+   window.requestFileSystem(type, size, successCallback, errorCallback)
+
+   function successCallback(fs) {
+      fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
+         alert('File creation successfull!')
+      }, errorCallback);
+   }
+
+   function errorCallback(error) {
+      alert("ERROR: " + error.code)
+   }
+
+}
+
+function writeFile() {
+   var type = window.TEMPORARY;
+   var size = 5*1024*1024;
+
+   window.requestFileSystem(type, size, successCallback, errorCallback)
+
+   function successCallback(fs) {
+
+      fs.root.getFile('log.txt', {create: true}, function(fileEntry) {
+
+         fileEntry.createWriter(function(fileWriter) {
+            fileWriter.onwriteend = function(e) {
+               alert('Write completed.');
+            };
+
+            fileWriter.onerror = function(e) {
+               alert('Write failed: ' + e.toString());
+            };
+
+            var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
+            fileWriter.write(blob);
+         }, errorCallback);
+
+      }, errorCallback);
+
+   }
+
+   function errorCallback(error) {
+      alert("ERROR: " + error.code)
+   }
+
+}
